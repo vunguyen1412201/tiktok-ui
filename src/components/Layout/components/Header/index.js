7105228e7bdacdb6315016b06b11b0af6c0d12e4
vunguyenhoang2001@gmail.com <vunguyenhoang2001@gmail.com>
2022-07-11 10:20:32 +0700

    upload

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,4 +1,15 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+//bind casi object styles vao. tra ra 1 function cx
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <h2> Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}></div>
+        </header>
+    );
 }
+
 export default Header;
